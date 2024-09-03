a5d38f2ae963d7373ddd606694a5cc03fa31201f
Solyze <zsebbenog@gmail.com>
2024-09-03 06:58:32 +0200

    Fullbright :bulb:

--- src/client/java/net/solyze/devtools/client/DevToolsClient.java
+++ src/client/java/net/solyze/devtools/client/DevToolsClient.java
@@ -6,6 +6,8 @@ import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.item.TooltipContext;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.option.GameOptions;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.item.ItemStack;
@@ -16,6 +18,7 @@ import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.solyze.devtools.DevTools;
+import org.jetbrains.annotations.NotNull;
 import org.lwjgl.glfw.GLFW;
 
 import java.util.Arrays;
@@ -23,34 +26,63 @@ import java.util.List;
 
 public class DevToolsClient implements ClientModInitializer {
 
+    // TODO: Cleanup. Make classes for handling stuff bla bla you get it (I hope)
+
     public static DevToolsClient INSTANCE;
     private final KeyBinding showNBTKeyBinding;
+    private final KeyBinding toggleFullbrightKeyBinding;
     private boolean showNBT;
+    private boolean fullbrightEnabled;
+    private double previousGamma = -Integer.MAX_VALUE;
 
     public DevToolsClient() {
         DevTools.LOGGER.info("{} (Client) initializing...", DevTools.MOD_DISPLAY);
         showNBTKeyBinding = key("show-nbt", GLFW.GLFW_KEY_N, "tools");
+        toggleFullbrightKeyBinding = key("toggle-fullbright", GLFW.GLFW_KEY_B, "tools");
     }
 
     @Override
     public void onInitializeClient() {
         INSTANCE = this;
         KeyBindingHelper.registerKeyBinding(showNBTKeyBinding);
+        KeyBindingHelper.registerKeyBinding(toggleFullbrightKeyBinding);
         ClientTickEvents.END_CLIENT_TICK.register(this::onEndClientTick);
         ItemTooltipCallback.EVENT.register(this::showNBTTooltipCallback);
         DevTools.LOGGER.info("{} (Client) has been initialized.", DevTools.MOD_DISPLAY);
     }
 
     private void onEndClientTick(MinecraftClient client) {
+        if (client.options == null) return;
+        if (previousGamma == -Integer.MAX_VALUE) {
+            previousGamma = client.options.getGamma().getValue();
+        }
         if (showNBTKeyBinding.wasPressed()) toggleShowNBT(client);
+        if (toggleFullbrightKeyBinding.wasPressed()) toggleFullbright();
+    }
+
+    private void toggleFullbright() {
+        MinecraftClient client = MinecraftClient.getInstance();
+        GameOptions options = client.options;
+        if (fullbrightEnabled) {
+            options.getGamma().setValue(previousGamma);
+        } else {
+            options.getGamma().setValue(1000.0);
+        }
+        fullbrightEnabled = !fullbrightEnabled;
+        if (client.player == null) return;
+        sendToggleMsg(client.player, "toggle-fullbright", fullbrightEnabled);
     }
 
     private void toggleShowNBT(MinecraftClient client) {
         showNBT = !showNBT;
         if (client.player == null) return;
+        sendToggleMsg(client.player, "show-nbt", showNBT);
+    }
+
+    private void sendToggleMsg(@NotNull ClientPlayerEntity player, String id, boolean condition) {
         MutableText text = Text.translatable("text." + DevTools.MOD_ID
-                + ".show-nbt." + (showNBT ? "enabled" : "disabled"));
-        client.player.sendMessage(text.formatted(showNBT ? Formatting.GREEN : Formatting.GRAY), true);
+                + "." + id + "." + (condition ? "enabled" : "disabled"));
+        player.sendMessage(text.formatted(condition ? Formatting.GREEN : Formatting.GRAY), true);
     }
 
     private void showNBTTooltipCallback(ItemStack stack, TooltipContext context, List<Text> lines) {
@@ -70,6 +102,7 @@ public class DevToolsClient implements ClientModInitializer {
             } else if (element instanceof NbtList list) {
                 addListToTooltip(list, lines, prefix, key);
             } else {
+                assert element != null;
                 addSimpleElementToTooltip(element, lines, prefix, key);
             }
         }
--- src/main/java/net/solyze/devtools/DevTools.java
+++ src/main/java/net/solyze/devtools/DevTools.java
@@ -8,7 +8,7 @@ public class DevTools implements ModInitializer {
 
     public static final String MOD_ID = "solyze-fabric-dev-tools",
                                MOD_DISPLAY = "SolyzeFabricDevTools";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_DISPLAY);
 
     public DevTools() {
         LOGGER.info("{} initializing...", MOD_DISPLAY);
